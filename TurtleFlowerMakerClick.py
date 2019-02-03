2fe124365b1759f4a2e8dfcf2aa1dc380830a974
eschulzpsd <39837518+eschulzpsd@users.noreply.github.com>
2019-02-02 20:20:27 -0700

    Add files via upload

---
+++ TurtleFlowerMakerClick.py
@@ -0,0 +1,28 @@
+import turtle as t
+import random
+from itertools import cycle
+
+colors = cycle(['violet','pink','cyan','white','red','orange','yellow','green','blue','purple'])
+
+t.clear()
+t.bgcolor('black')
+t.speed(100)
+t.pensize(10)
+
+def draw_flower(x,y):
+    t.up()
+    t.pencolor(next(colors))
+    t.goto(x,y)
+    t.setheading(0)
+    t.down()
+    for i in range(5):
+        for i in range(35):
+            t.forward(3)
+            t.right(3)
+        t.right(75)
+        for i in range(35):
+            t.forward(3)
+            t.right(3)
+    
+t.onscreenclick(draw_flower)
+
